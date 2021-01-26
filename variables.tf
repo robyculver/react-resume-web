c331efa55d4c1227f5ca6cdfe52fc0ec1f1e4c61
robyculver <robyculver@gmail.com>
2021-01-26 14:46:49 -0500

    Delete variables.tf

--- variables.tf
+++
@@ -1,9 +0,0 @@
-  
-variable "heroku_pipeline_name" {}
-variable "heroku_stack" {}
-variable "heroku_api_key" {}
-variable "heroku_user" {}
-variable "heroku_dev_app" {}
-variable "heroku_prod_app" {}
-variable "heroku_region" {}
-variable "heroku_app_buildpacks" { type = list(string) }
